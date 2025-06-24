073ff93583e35db014313ed2c27aa93566c1cf78
ral1981 <rolfalsemgeest@gmail.com>
2025-06-24 16:54:32 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+//import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://vrasvqmlpdqogghtkqfu.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYXN2cW1scGRxb2dnaHRrcWZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNTQ5ODgsImV4cCI6MjA2NTkzMDk4OH0.K5wuITvCrQdCvfsRRpWZnFtZ3cvHLAdXhudBxiP9hAc'; // Keep secret keys safe in prod
